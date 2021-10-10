45fe721a1994e7bf36157b16972bffb7926a8ad5
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2021-10-10 10:44:28 +0800

    update pictures

--- R/lyrics.R
+++ R/lyrics.R
@@ -8,6 +8,8 @@ library(textdata)
 get_sentiments("afinn")
 
 # read lyrics and unnest text into word
+# number is the position of each song in the album
+# number>0 denotes the score for a song
 lyrics <- read_csv(file="./Data/lyrics.csv") %>% 
   group_by(Album) %>% 
   mutate(Number = row_number()) %>% 
@@ -15,8 +17,7 @@ lyrics <- read_csv(file="./Data/lyrics.csv") %>%
   unnest_tokens(word,Lyrics) 
 
 # calculate average sentiment for each album 
-# create number=0 to position the G musical symbol
-# (mean score of the album) at beginning of each album
+# number = 0 denotes the score for an album
 afinn_album <- lyrics %>% 
   inner_join(get_sentiments("afinn")) %>% 
   group_by(Album, Year) %>% 
@@ -30,12 +31,16 @@ afinn_song <- lyrics %>%
   group_by(Album, Year, Song, Number) %>% 
   summarise(sentiment = mean(value))
 
-
+# union album and song into afinn
+# the data are not tidy
+# but it is to trick Tableau to display the musical symbols:
+# create number=0 to position the G musical symbol
+# (mean score of the album) at beginning of each album
 afinn <- rbind(afinn_album, afinn_song) %>% 
   arrange(Album, Number) %>% 
   mutate(symbol=if_else(Number == 0, "mean", 
                         if_else(sentiment>0, "positive", 
                                 if_else(sentiment<0, "negative", "zero"))))
   
-# write into csv file
+# write output into csv file
 write_csv(afinn,file="./Data/afinn.csv")
